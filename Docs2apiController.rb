eaa0eab5a818be02207fea02197fe0af8fcf1de7
Jascha <jascha@zaaksysteem.nl>
2018-03-22 10:43:02 +0100

    First fully functional version. Horrible code, but it works! No logging yet.

--- Docs2apiController.rb
+++ Docs2apiController.rb
@@ -3,17 +3,24 @@ require 'csv'
 require 'json'
 # require 'HTTParty'
 require 'typhoeus'
+require 'logger'
 
 # global app variables
 @status_message = ""
 # end global app variables
 
+
+
+
+log = Logger.new('log.txt')
+log.debug "Log file created"
+
+
+
 class Docs2apiController < Sinatra::Base
   post "/docs2api" do
-    puts Dir.pwd 
     tempfile = params['CSV_FILE'][:tempfile]
     @filename = params['CSV_FILE'][:filename]
-
     File.open("public/files/testdump/#{@filename}", "r+") do |f|
       f.write(tempfile.read)
     end
@@ -25,9 +32,11 @@ class Docs2apiController < Sinatra::Base
     API_KEY = params[:API_KEY_Target]
     Endpoint_URL2 = params[:Endpoint_URL2]
 
+
     # Now we read the CSV
     data = CSV.read("public/files/testdump/#{@filename}", { encoding: "UTF-8", headers: true, header_converters: :symbol }) # converters: :all
 
+    # all the hash functions are probably a method?
     # turn the CSV into a ruby hash
     hashed_data = data.map { |d| d.to_hash }
 
@@ -36,80 +45,94 @@ class Docs2apiController < Sinatra::Base
 
     # Transforms the individual values, so they're hashes:
     hashed_data.each { |f| f.transform_values! { |value| Array(value) } }
-    
-    puts hashed_data
+
+   
 
 
     # Now we can define the request body as: casetype_id + requestor + values
-    hashed_data.each_with_index do |valuesss, _index|
-      pre_request_body = {}
-      pre_request_body[:casetype_id] = casetype_id
-      pre_request_body[:requestor] = valuesss[:clientnummer]
-      pre_request_body[:source] = "behandelaar"
-      pre_request_body[:values] = valuesss[:dossier_path]  # Figure out how to add filelocation here
+    hashed_data.each_with_index do |csvvalues, _index|
+    
+    Dossier_Path = csvvalues[:dossier_path][0]
 
-      request_body = pre_request_body.to_json
-
-      # Create cases
-      # responsa = HTTParty.post(
-      #   "#{Endpoint_URL2}/api/v1/case/create/",
-      #   :body => request_body,
-      #   :debug_output => $stdout,
-      #   :headers => { 'Content-Type' => 'application/json', "API-key" => "#{API_KEY}", "API-Interface-ID" => "#{API_Interface_ID}" }
-      #   )
-
-      # print "Here is your response: #{responsa['status_code']}"
-      # print responsa['status_code']
-      # print responsa['result']['instance']['message']
-      # print "____End response___"
-      
-      # Now we need to prepare_file all the files + save them sexy file id's
-      puts "_____Dossier path goes here:_____"      
-      puts valuesss[:dossier_path][0]
-
-      Dossier_Path = valuesss[:dossier_path][0]
-      puts "_____Current path is:_____"  
-      puts Dir.pwd
-      
-      puts "_____Change dir_____"       
+      # Now we need to prepare_file all the files + save them sexy file id's     
       Dir.chdir "public/files/testdump/#{Dossier_Path}" do
-        puts "_____Files in dir are:_____"  
+        
+        # dit is een method
+        filecode_array = Array.new
         Dir.glob("*.pdf") {|filenamer|
-          puts filenamer
+            # this should be a generic method for file prep, where we define the body before we run the method
+            responsb = Typhoeus.post(
+            "#{Endpoint_URL2}/api/v1/case/prepare_file/",
+            #method: :post,
+            verbose: true,
+            body: {
+              upload: File.open(filenamer)
+            },
+            headers: { 'Content-Type' => "multipart/form-data", "API-key" => "#{API_KEY}", "API-Interface-ID" => "#{API_Interface_ID}" }
+            ) 
 
-          puts "Posting to api...."
-          responsb = Typhoeus.post(
-          "#{Endpoint_URL2}/api/v1/case/prepare_file/",
+          bla = JSON.parse(responsb.body)
+          
+          # puts bla['result']['instance']['references'].keys
+          filecode_array << bla['result']['instance']['references'].keys[0]
+          
+          # puts filecode_array
+
+          # dit is een method
+          values = Hash.new
+          values[:bto_clientnummer] = csvvalues[:clientnummer]
+          values[:ztc_brondocument] = filecode_array
+          # puts "=====Values:====="
+          # puts values
+          # puts csvvalues[:clientnummer]
+          # puts values[:bto_clientnummer]
+          # puts "================="
+
+          # bloop = values[:bto_clientnummer]
+          requestor = Hash.new
+          requestor[:id] = csvvalues[:clientnummer]
+          requestor[:type] = "person"
+
+          puts requestor
+
+          #this prerequest stuff should be a method that works with params, probably?
+          pre_request_body = {}
+          pre_request_body[:casetype_id] = casetype_id
+          pre_request_body[:requestor] = requestor
+          pre_request_body[:source] = "behandelaar"
+          pre_request_body[:values] = values
+          # request_body = pre_request_body.to_json
+
+
+          # pre_request_body[:values]['ztc_documentkenmerk'] = "#{filecode_array}"
+          request_body = pre_request_body.to_json
+          # puts request_body
+
+          # this should be a generic method for case creation, where we define the body before we run the method
+          responsc = Typhoeus.post(
+          "#{Endpoint_URL2}/api/v1/case/create/",
           #method: :post,
           verbose: true,
-          body: {
-            upload: File.open(filenamer)
-            },
-          headers: { 'Content-Type' => "multipart/form-data", "API-key" => "#{API_KEY}", "API-Interface-ID" => "#{API_Interface_ID}" }
+          body: request_body,
+          headers: { 'Content-Type' => "application/json", "API-key" => "#{API_KEY}", "API-Interface-ID" => "#{API_Interface_ID}" }
           ) 
 
-          # p responsb.code
-          # p responsb.headers
-          # p responsb.body
-          bla = JSON.parse(responsb.body)
-          filecode_array = Array.new
-          puts bla['result']['instance']['references'].keys
-          filecode_array << bla['result']['instance']['references'].keys
+
+          # bla = JSON.parse(responsc)
+          puts "==== RESPONSE HERE: ===="
+          # puts bla
+          puts responsc.code
+          puts responsc.headers
+          puts "========================"
+
+          # responsb = HTTParty.post(
+          # "#{Endpoint_URL2}/api/v1/case/create/",
+          # :body => request_body,
+          # :debug_output => $stdout,
+          # :headers => { 'Content-Type' => 'application/json', "API-key" => "#{API_KEY}", "API-Interface-ID" => "#{API_Interface_ID}" }
+          # )
+      }
           
-          puts "00000000000000000000000000000000000000000000000"
-          puts "__________ Puts the Filecode Array: ___________"
-          puts filecode_array
-          puts "00000000000000000000000000000000000000000000000"
-       }
-
-      # responsb = HTTParty.post(
-      #   "#{Endpoint_URL2}/api/v1/case/create/",
-      #   :body => request_body,
-      #   :debug_output => $stdout,
-      #   :headers => { 'Content-Type' => 'application/json', "API-key" => "#{API_KEY}", "API-Interface-ID" => "#{API_Interface_ID}" }
-      #   )
-
-      # Now we need to update the case with all the id's 
 
       end
       # @status_message = "Tapir says: #{responsa['status_code']} - #{responsa['result']['instance']['message']}"
